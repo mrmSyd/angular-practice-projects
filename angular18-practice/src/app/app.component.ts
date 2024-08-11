ad5dffc1d962a4142caa91af114de9dba25fe6fc
Maryam Sayyed <maryamcs72@gmail.com>
2024-08-11 23:05:14 +0530

    One way data binding: 1. Interpolation, 2. Property Binding

--- angular18-practice/src/app/app.component.ts
+++ angular18-practice/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from "./Components/add-employee/add-employee.component";
 import { EmployeeListComponent } from "./Components/employee-list/employee-list.component";
+import { DataBindingComponent } from "./Components/data-binding/data-binding.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent],
+  imports: [RouterOutlet, AddEmployeeComponent, EmployeeListComponent, DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
