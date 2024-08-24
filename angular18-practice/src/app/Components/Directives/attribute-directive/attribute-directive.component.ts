94753f47af3e945582245a6dc79e3ea5d1fcbc0d
Maryam Sayyed <maryamcs72@gmail.com>
2024-08-24 20:31:36 +0530

    Attribute Directive: [ngStyle]

--- angular18-practice/src/app/Components/Directives/attribute-directive/attribute-directive.component.ts
+++ angular18-practice/src/app/Components/Directives/attribute-directive/attribute-directive.component.ts
@@ -12,6 +12,22 @@ export class AttributeDirectiveComponent {
  
   bgHouseColor = "bg-primary";
   isGreenHouse = false;
+  studentList: any[] = [
+    { name: 'Harry Potter', house: 'Godric Gryffindor', isCaptain: false, marks: 50},
+    { name: 'Ronald Weasley', house: 'Godric Gryffindor', isCaptain: false, marks: 45 },
+    { name: 'Hermione Granger', house: 'Godric Gryffindor', isCaptain: false, marks: 98 },
+    { name: 'Draco Malfoy', house: 'Salazar Slytherin', isCaptain: true, marks: 75 },
+    { name: 'Luna Lovegood', house: 'Rowena Ravenclaw', isCaptain: false, marks: 80 },
+    { name: 'Cedric Diggory', house: 'Helga Hufflepuff', isCaptain: true, marks: 85 },
+  ]
+  customStyle: any = {
+    'color': 'white',
+    'background-color': 'lavender',
+    'width': '100px',
+    'height': '100px',
+    'font-size': '25px',
+    'border': '2px solid black'
+  }
 
   changeHouse(){
     this.bgHouseColor = "bg-success";
